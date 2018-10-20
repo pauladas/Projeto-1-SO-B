5706e8a62c3bbf83851c87dd152f49e578300b9b
Vinicius Trevisan <36710856+vinicius-trev@users.noreply.github.com>
2018-10-20 17:43:19 -0300

    Add files via upload

--- cryptomodule.c
+++ cryptomodule.c
@@ -6,18 +6,21 @@
  *  Vinícius Trevisan   - 16011231
  */
 
-#include <linux/init.h>           /* Macros Utilizadas para iniciar (__init) e terminar (__exit) o modulo */
-#include <linux/module.h>         /* Biblioteca para carregar o modulo de kernel no sistema */
-#include <linux/device.h>         /* Biblioteca para suportar o modulo de dispositivo */
-#include <linux/kernel.h>         /* Contem as funcoes macros e tipos de estruturas do kernel */
-#include <linux/moduleparam.h>    /* Contem a funcao para receber parametros durante a inicializacao do modulo */
-#include <linux/stat.h>
-#include <crypto/internal/hash.h> /* Utilizada para a funcao de HASH */
-#include <linux/string.h>         /* Biblioteca para manipular strings em nivel de kernel */
-#include <linux/fs.h>             /* Biblioteca para gerenciamento de sistema de arquivos (abrir,fechar, ler e escrever em arquivo) */
-#include <linux/uaccess.h>        /* Necessario para copiar os dados para o espaco de usuario*/
-#define DEVICE_NAME "crypto"      /* Nome do dispositivo que aparecera em /proc/devices (sera o nome do arquivo criado com mknode /dev/crypto c MAJOR 0) */
-#define CLASS_NAME "cryptomodule" /* Definicao da classe do dispositivo */
+ #include <linux/init.h>                /* Macros Utilizadas para iniciar (__init) e terminar (__exit) o modulo */
+ #include <linux/module.h>              /* Biblioteca para carregar o modulo de kernel no sistema */
+ #include <linux/device.h>              /* Biblioteca para suportar o modulo de dispositivo */
+ #include <linux/kernel.h>              /* Contem as funcoes macros e tipos de estruturas do kernel */
+ #include <crypto/internal/skcipher.h>  /* Biblioteca para encriptar e decriptar */
+ #include <linux/crypto.h>
+ #include <linux/moduleparam.h>         /* Contem a funcao para receber parametros durante a inicializacao do modulo */
+ #include <linux/stat.h>
+ #include <crypto/internal/hash.h>      /* Utilizada para a funcao de HASH */
+ #include <linux/string.h>              /* Biblioteca para manipular strings em nivel de kernel */
+ #include <linux/fs.h>                  /* Biblioteca para gerenciamento de sistema de arquivos (abrir,fechar, ler e escrever em arquivo) */
+ #include <linux/uaccess.h>             /* Necessario para copiar os dados para o espaco de usuario*/
+ #define DEVICE_NAME "crypto"           /* Nome do dispositivo que aparecera em /proc/devices (sera o nome do arquivo criado com mknode /dev/crypto c MAJOR 0) */
+ #define CLASS_NAME "cryptomodule"      /* Definicao da classe do dispositivo */
+
 #define KEY_SIZE 256
 #define SHA256_LENGTH KEY_SIZE / 8
 
@@ -34,10 +37,12 @@ MODULE_VERSION("1.0");                                               /* Varsao *
 static int qtdBlocos;
 static int majorNumber;                                 /* Armazena o numero major definido automaticamente pelo driver */
 static int size_of_message;                             /* Guarda o tamanho da entrada quando o usuario grava alguma coisa no arquivo do modulo */
+static int size_of_key;
+static int size_of_output;
 static char operacao;                                   /* Indica a operacao a ser realizada c - d - h */
 static char *key;                                       /* Armazena a chave em CARACTERES passada como parametro na insercao do modulo -- Durante a conversao, o limite definido foi de 64 caracteres hexa de entrada ou seja, 32 bytes*/
-static char keyHexa[KEY_SIZE / 8];                      /* Armazena a chave HEXADECIMAL convertida a partir da chave em CARACTERES */
-static char keyChar[KEY_SIZE / 4 + 1];                  /* Representacao em caracteres da chave considerada em hexadecimal */
+static char keyHexa[(KEY_SIZE / 8) + 1];                      /* Armazena a chave HEXADECIMAL convertida a partir da chave em CARACTERES */
+static char keyChar[(KEY_SIZE / 4) + 1];                  /* Representacao em caracteres da chave considerada em hexadecimal */
 static char mensagemHexaInput[(KEY_SIZE / 8) * 5 + 1];  /* Armazena a mensagem lida do arquivo do modulo em forma hexadecimal para ser utilizado na criptografia, descriptografia ou hash obs: vezes 5 pois definimos que iremos realizar as operacoes com no maximo 5 blocos (160 bytes)*/
 static char mensagemHexaOutput[(KEY_SIZE / 8) * 5 + 1]; /* Armazena o resultado da criptogragia, descriptografia ou hash em hexadecimal */
 static char mensagemCharInput[(KEY_SIZE / 4) * 5 + 1];  /* Armazena a mensagem em CARACTERES lida do arquivo do modulo. Obs: o tamanho eh "ilimitado", mas iremos considerar os primeiros 64*5(blocos) caracteres*/
@@ -45,15 +50,20 @@ static char mensagemCharOutput[(KEY_SIZE / 4) * 5 + 1]; /* Armazena o resultado 
 static int numUtil = 0;                                 /* Contador do numero de vezes que o arquivo de dispositivo foi aberto */
 static struct device *cryptomoduleDevice = NULL;        /* Ponteiro para a struct do driver de dispositivo */
 static struct class *cryptomoduleClass = NULL;          /* Ponteiro para a struct da classe representando o driver de dispositivo */
+static struct skcipher_def sk;                          /* estrutura para funcao de encriptar */
 
 /* Prototipos das funcoes */
 static int      dev_abrir(struct inode *, struct file *);
 static int      dev_fechar(struct inode *, struct file *);
 static ssize_t  dev_leitura(struct file *, char *, size_t, loff_t *);
 static ssize_t  dev_escrita(struct file *, const char *, size_t, loff_t *);
-static void     converterChar2Hexa(char *pChar, char *pHexa, int qtdBlocos);
-static void     converterHexa2Char(char *pHexa, char *pChar, int qtdBlocos);
-static int      cryptosha256(char *pData, int *pResultado, int tamanhoData);
+static void     converterChar2Hexa(char *pChar, char *pHexa);
+static void     converterHexa2Char(char *pHexa, char *pChar);
+static int      cryptosha256(char *pData, char *pResultado);
+static void 	  test_skcipher_finish(struct skcipher_def * sk);
+static int 	    test_skcipher_result(struct skcipher_def * sk, int rc);
+static void 	  test_skcipher_callback(struct crypto_async_request *req, int error);
+static int 	    test_skcipher_encrypt(char * plaintext, char * password, struct skcipher_def * sk);
 
 /* Obtendo os parametros passados na inicializacao */
 /* para carregar o modulo: insmod cryptomodule.ko key="ABCDEF12345667890" */
@@ -110,10 +120,11 @@ static int __init cryptomodule_init(void)
   }
   /* Finalmente o dispositivo foi iniciado */
   pr_info("cryptoModule: Dispositivo criado com sucesso\n");
-
+  size_of_key = strlen(key);
+  size_of_message = size_of_key;
   pr_info("cryptoModule: Chave (Key) BRUTA recebida: %s\n", key);
-  converterChar2Hexa(key, keyHexa, 1);     /* Salva em keyHexa a sequencia de bytes que representa os caracteres em hexadecimal lidos no carregamento do modulo */
-  converterHexa2Char(keyHexa, keyChar, 1); /* Salva em keyChar os caracteres que representam a chave em hexadecimal */
+  converterChar2Hexa(key, keyHexa);     /* Salva em keyHexa a sequencia de bytes que representa os caracteres em hexadecimal lidos no carregamento do modulo */
+  converterHexa2Char(keyHexa, keyChar); /* Salva em keyChar os caracteres que representam a chave em hexadecimal */
   pr_info("cryptoModule: Chave (Key) CONSIDERADA em hexadecimal: %s\n", keyChar);
 
   return 0; /* Retorno igual a 0 (sucesso) */
@@ -155,18 +166,16 @@ static ssize_t dev_leitura(struct file *filep, char *buffer, size_t len, loff_t 
 {
   int contador_erro = 0;
 
-  if (operacao == 'h')
-    qtdBlocos = 1;
-
   /* Para enviar os dados para o usuario, temos que converter os dados em hexadecimal mensagemHexaOutput para mensagemCharOutput */
   /* Para isso */
-  converterHexa2Char(mensagemHexaOutput, mensagemCharOutput, qtdBlocos);
+  converterHexa2Char(mensagemHexaOutput, mensagemCharOutput);
+
 
   /* A funcao copy_to_user retorna 0 quando eh sucedida, sua estrutura eh:
     * copy_to_user(ponteiro para onde, ponteiro de onde, tamanho em bytes)
     */
-  mensagemCharOutput[(qtdBlocos * 64)] = '\0';
-  contador_erro = copy_to_user(buffer, mensagemCharOutput, 64 * qtdBlocos + 1); /* estou copiando para buffer size_of_message bytes de message */
+  mensagemCharOutput[size_of_output] = '\0';
+  contador_erro = copy_to_user(buffer, mensagemCharOutput, size_of_output+1); /* estou copiando para buffer size_of_message bytes de message */
 
   if (contador_erro == 0) /* Entrara nesse if caso a copia seja efetuada com sucesso */
   {
@@ -190,27 +199,30 @@ static ssize_t dev_leitura(struct file *filep, char *buffer, size_t len, loff_t 
  */
 static ssize_t dev_escrita(struct file *filep, const char *buffer, size_t len, loff_t *offset)
 {
-  int retorno;
+  int retorno,i;
+  /* resetando todas as variaveis */
+  memset(mensagemCharInput,0,64*5);
+  memset(mensagemCharOutput,0,64*5);
+  memset(mensagemHexaInput,0,32*5);
+  memset(mensagemHexaOutput,0,32*5);
 
-  sprintf(&operacao, "%c", buffer[0]); // Pega a opcao de operacao
-  size_of_message = len - 2;           // Armazena o tamanho da chave recebida
 
   /* mensagemCharInput nao pode ser maior do que 64*5 caracteres, caso for, somente consideramos os 64*5 caracteres iniciais */
   if (size_of_message > 64 * 5)
     size_of_message = 64 * 5;
-  snprintf(mensagemCharInput, size_of_message + 1, "%s", buffer + 2); // buffer + 2 para nao pegar o caractere de operacao (+1 elimina o caractere de controle +1 elimina o espaco)
-  printk(KERN_INFO "cryptoModule: Recebeu do arquivo a operacao:%c e dado:%s\n", operacao, mensagemCharInput);
 
   /* Convertendo o dado de entrada para hexa - Definimos no maximo 5 blocos de dados */
   /* Quebrando a mensagemCharInput em blocos de 32 bytes */
   /* Convertendo cada bloco de 64 caracteres para um de 32 bytes hexadecimal mensagemCharInput (64B) -> mensagemHexaInput(32B) */
   qtdBlocos = size_of_message / 64;
-  if (size_of_message % 64 != 0)
-    qtdBlocos += 1;
+  copy_from_user(mensagemCharInput,buffer,len);
+  mensagemCharInput[len] = '\0';
+  size_of_message = len-2;           // Armazena o tamanho da chave recebida
+  sprintf(&operacao, "%c", mensagemCharInput[0]); // Pega a opcao de operacao
+  strcpy(mensagemCharInput,mensagemCharInput+2);
 
-  converterChar2Hexa(mensagemCharInput, mensagemHexaInput, qtdBlocos); /* bloco menos significativo (0 1 2 3 4) */
-  converterHexa2Char(mensagemHexaInput, mensagemCharOutput, qtdBlocos);
-
+  converterChar2Hexa(mensagemCharInput, mensagemHexaInput);
+  converterHexa2Char(mensagemHexaInput, mensagemCharOutput);
   printk(KERN_INFO "cryptoModule: Recebeu do arquivo a operacao:%c e dado convertido para Hexa: %s", operacao, mensagemCharOutput);
 
   /* Para as operacoes, a entrada sera a mensagemHexaInput e a saida devera obrigatoriamente estar contida em mensagemHexaOutput */
@@ -227,8 +239,9 @@ static ssize_t dev_escrita(struct file *filep, const char *buffer, size_t len, l
   case 'h': /* Aqui eh o codigo responsavel por Calcular o hash da mensagemHexaInput e salvar em mensagemHexaOutput*/
     /* em tese, um hash nao possui limite de entrada, porem, nesse modulo, ela foi limitada a 64*5=320 caracteres */
     printk("cryptoModule: Calcular Hash\n");
-    retorno = cryptosha256(mensagemHexaInput, mensagemHexaOutput, size_of_message); /* Aqui size_of_message contem quantos caracteres o mensagemHexaInput possui convertidos para hexadecimal */
-    size_of_message = 64; /* A saida do hash eh sempre de mesmo tamanho */
+    cryptosha256(mensagemHexaInput, mensagemHexaOutput); /* Aqui size_of_message contem quantos caracteres o mensagemHexaInput possui convertidos para hexadecimal */
+    size_of_output = 64; /* A saida do hash eh sempre de mesmo tamanho */
+    size_of_message=64;
     break;
 
   default:
@@ -250,15 +263,15 @@ static int dev_fechar(struct inode *inodep, struct file *filep)
 }
 
 /* Funcao para converter um array de caracteres para um array em hexadecimal */
-static void converterChar2Hexa(char *pChar, char *pHexa, int qtdBlocos)
+static void converterChar2Hexa(char *pChar, char *pHexa)
 {
-  /* Ideia: caso o numero for entre 0 e 9 subtrair 48, caso for de A a F subtrair 65. Logo, realizar um OR & com o byte, shift para a direita 4x e adicionar o segundo numero */
-  memset(pHexa, 0, (KEY_SIZE / 8) * qtdBlocos);                                                                                                   /* Zerando o conteudo da memoria do array keyHexa -> Garante que nao existira lixo na memoria */
-  int lengthChave = ((strlen(pChar) >= (KEY_SIZE / 4) * qtdBlocos) ? (lengthChave = (KEY_SIZE / 4) * qtdBlocos) : (lengthChave = strlen(pChar))); /* Variavel para armazenar a qtd de caracteres na chave. Caso tenha mais de 64 soh os 64 primeiros sao considerados */
-  int j = 32 * qtdBlocos - ((lengthChave % 2 == 0) ? (lengthChave / 2) : ((lengthChave / 2) + 1));                                                /* indice para acessar o vetor keyHexa a partir da posicao inicial correta*/
-  int i = 0;                                                                                                                                      /* indice para acessar a chave de caractere lida na insercao do modulo */
-  /* A fim de normalizar o numero, caso a quantidade de caracteres for impar, o primeiro caractere da chave devera ocupar o primeiro byte de keyHexa sozinho */
-  if (lengthChave % 2 != 0) /* ou seja, o numero de caracteres eh impar */
+  /* Ideia: Cada byte de pchar sera convertido em dois digitos hexa em cada posicao de pHexa, ou seja, o tamanho de pchar eh o dobro de phexa */
+  /* o Padding sera feito na direita */
+  int i;
+  int j=0;
+  /* Zerando o conteudo da memoria do array keyHexa -> Garante que nao existira lixo na memoria */
+  /* Seta todas as posicoes com 0 -- size_of_messagesera OBRIGATORIAMENTE PAR, ja que cada digito hexa correspondem a dois numeros */
+  for(i=0; i<size_of_message;i++)
   {
     if (pChar[i] >= 48 && pChar[i] <= 57) /* o caractere da chave eh entre 0(ASCII 48) e 9(ASCII 57) */
     {
@@ -273,70 +286,47 @@ static void converterChar2Hexa(char *pChar, char *pHexa, int qtdBlocos)
       pHexa[j] = pHexa[j] | (pChar[i] - 55); /* Subtrai 55 para obter o valor do caractere em seu numero correspondente (entre 10 e 15) e manipula os bits com OR*/
     }
 
-    /* Como a quantidade de caracteres eh impar e o primeiro caractere foi salvo em um bit isolado, devemos incrementar os contadores */
-    i++; /* incrementa o contador de caracteres da chave */
-    j++; /* incrementa uma posicao de keyHexa, ja que o promeiro caractere tem q estar sozinho no byte */
+    if (i % 2 == 0) /* O shift de 4 posicoes ocorrera quando o indice i, ou seja, o apontador para a chave em caracteres for PAR */
+    {
+      pHexa[j] = pHexa[j] << 4;
+    }
+    else /* Caso o contador da posicao do vetor de caracteres nao for par, ou seja, ja cadastramos o segundo digito ...*/
+    {
+      j++; /*... pula para o proximo byte de keyHexa */
+    }
   }
+  pHexa[j+1]='\0';
 
-  /* Continuando a conversao do numero caso a quantidade de caracteres for impar ou iniciando a conversao caso a quantidade de caracteres for par */
-  for (i; ((i < lengthChave) && i < ((KEY_SIZE / 4) * qtdBlocos)); i++) /* o indice i vai de 0 a 63 no maximo, ou seja, caso a chave for maior que 64 caracteres, soh pega os 64 primeiros */
-  {
-    if (pChar[i] >= 48 && pChar[i] <= 57) /* o caractere da chave eh entre 0(ASCII 48) e 9(ASCII 57) */
-    {
-      pHexa[j] = pHexa[j] | (pChar[i] - 48); /* Subtrai 48 para obter o valor do caractere em seu numero correspondente e manipula os bits com OR*/
-    }
-    else if (pChar[i] >= 97 && pChar[i] <= 102) /* o caractere da chave eh entre a(ASCII 97) e f(ASCII 102) */
-    {
-      pHexa[j] = pHexa[j] | (pChar[i] - 87); /* Subtrai 87 para obter o valor do caractere em seu numero correspondente (entre 10 e 15) e manipula os bits com OR*/
-    }
-    else /* o caractere da chave eh entre A(ASCII 65) e F(ASCII 70) */
-    {
-      pHexa[j] = pHexa[j] | (pChar[i] - 55); /* Subtrai 55 para obter o valor do caractere em seu numero correspondente (entre 10 e 15) e manipula os bits com OR*/
-    }
-    /* Definindo quando ocorre o shift no byte para armazenar o segundo numero no mesmo byte */
-    if (lengthChave % 2 != 0) /* Caso a quantidade de caracteres for IMPAR */
-    {
-      if (i % 2 != 0) /* O shift de 4 posicoes ocorrera quando o indice i, ou seja, o apontador para a chave em caracteres for IMPAR */
-      {
-        pHexa[j] = pHexa[j] << 4;
-      }
-      else /* Caso o contador da posicao do vetor de caracteres nao for impar, ou seja, ja cadastramos o segundo digito ...*/
-      {
-        j++; /*... pula para o proximo byte de keyHexa */
-      }
-    }
-    else /* Caso a quantidade de caracteres for PAR */
-    {
-      if (i % 2 == 0) /* O shift de 4 posicoes para a direita ocorrera quando o indice i for PAR */
-      {
-        pHexa[j] = pHexa[j] << 4;
-      }
-      else /* Caso o contador de posicao do vetor de caracteres NAO FOR PAR, ou seja, ja cadastramos o segundo digito ... */
-      {
-        j++; /*... pula para o proximo byte de keyHexa */
-      }
-    }
-  }
 }
 
 /* Funcao para converter um array de hexadecimais para uma string de caracteres */
-static void converterHexa2Char(char *pHexa, char *pChar, int qtdBlocos)
+static void converterHexa2Char(char *pHexa, char *pChar)
 {
   int i;
-  memset(pChar, 0, (KEY_SIZE / 4) * qtdBlocos); /* Zerando o conteudo da memoria do array keyHexa -> Garante que nao existira lixo na memoria */
-  for (i = 0; i < (KEY_SIZE / 8) * qtdBlocos; i++)
-    sprintf(&pChar[i * 2], "%02x", (unsigned char)pHexa[i]);
-  pChar[(KEY_SIZE / 4) * qtdBlocos] = '\0';
+  for (i = 0; i < size_of_message/2; i++)
+  {
+    sprintf(&pChar[i * 2], "%02hhx", (unsigned char)pHexa[i]);
+  }
+  pChar[i*2] = '\0';
+
 }
 
-static int cryptosha256(char *pData, char *pResultado, int tamanhoData)
+static int cryptosha256(char *pData, char *pResultado)
 {
+
     char *plaintext;                 /* String no qual o hash sera feito */
     char hash_sha256[SHA256_LENGTH]; /* String contendo o resultado da operacao hash */
     struct crypto_shash *sha256;     /* Objeto de transformacao da api do hash -> uma struct que armazena os dados da operacao hash */
     struct shash_desc *shash;        /* Struct com o objeto de transformacao e flags do hash */
+    int qtdBytes = (size_of_message % 2 == 0) ? (qtdBytes=size_of_message) : (qtdBytes=size_of_message+1);
 
-    //plaintext=pData+;
+    plaintext = (char *)kmalloc(size_of_message/2,GFP_KERNEL);
+    strcpy(plaintext,pData);
+
+    printk("plaintext:");
+    int i;
+    for (i = 0; i < size_of_message/2+5; i++)
+      printk("%02hhx", (unsigned char)plaintext[i]);
 
     sha256 = crypto_alloc_shash("sha256", 0, 0); /* Indica que o hash feito sera o SHA256 */
 
@@ -361,8 +351,8 @@ static int cryptosha256(char *pData, char *pResultado, int tamanhoData)
         return -1;
 
     kfree(shash);                             /* Desaloca a estrturura alocada dinamicamente para realizar o hash */
+    kfree(plaintext);
     crypto_free_shash(sha256);                /* Libera o objeto de hash sha256 */
-    show_hash_result(plaintext, hash_sha256); /* Exibe/copia o resultado do hash para hash_sha256 */
     memcpy(pResultado,hash_sha256,32);        /* Copia o resultado do hash para mensagemHexaOutput */
 
     return 0;
